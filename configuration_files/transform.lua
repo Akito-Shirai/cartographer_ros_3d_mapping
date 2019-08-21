d8ccc9b710cbff376253f067b8845ced8af9ccdf
Akito-Shirai <tnbc080527@gmail.com>
2019-08-21 16:13:35 +0900

    Add a file

---
+++ configuration_files/transform.lua
@@ -0,0 +1,14 @@
+XY_TRANSFORM =  {
+  translation = { 0., 0., 0. },
+  rotation = { 0., -math.pi / 2., 0., },
+}
+
+XZ_TRANSFORM =  {
+  translation = { 0., 0., 0. },
+  rotation = { 0. , 0., -math.pi/2, },
+}
+
+YZ_TRANSFORM =  {
+  translation = { 0., 0., 0. },
+  rotation = { 0. , 0., math.pi, },
+}
